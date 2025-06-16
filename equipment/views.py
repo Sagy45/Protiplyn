27ad07a70469e809f8ce7440fb64ee7de1183034
TMAREK <tmarek@seznam.cz>
2025-06-16 14:54:29 +0200

    new views and urls for equipments

--- equipment/views.py
+++ equipment/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from .models import EquipmentType
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+
+from viewer.models import Station
+from .models import EquipmentType, VehicleStorage, Mask, ADPMulti, ADPSingle, AirTank, PCHO, PA, Complete
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
 from django.urls import reverse_lazy
 
 class EquipmentTypeListView(ListView):
@@ -27,3 +29,237 @@ class EquipmentTypeDeleteView(DeleteView):
     model = EquipmentType
     template_name = 'equipment/equipmenttype_confirm_delete.html'
     success_url = reverse_lazy('equipmenttype_list')
+
+
+class VehicleStorageListView(ListView):
+    model = VehicleStorage
+    template_name = 'vehicle/vehicle_list.html'
+    context_object_name = 'vehicle_storages'
+
+
+class VehicleStorageCreateView(CreateView):
+    model = VehicleStorage
+    fields = '__all__'
+    template_name = 'vehicle/vehicle_form.html'
+    success_url = reverse_lazy('vehicle_list')
+
+
+class VehicleStorageUpdateView(UpdateView):
+    model = VehicleStorage
+    fields = '__all__'
+    template_name = 'vehicle/vehicle_form.html'
+    success_url = reverse_lazy('vehicle_list')
+
+
+class VehicleStorageDeleteView(DeleteView):
+    model = VehicleStorage
+    template_name = 'vehicle/vehicle_confirm_delete.html'
+    reverse_url = reverse_lazy('vehicle_list')
+
+
+class MaskListView(ListView):
+    model = Mask
+    template_name = 'mask/mask_list.html'
+    context_object_name = 'masks'
+
+
+class MaskCreateView(CreateView):
+    model = Mask
+    fields = '__all__'
+    template_name = 'mask/mask_form.html'
+    success_url = reverse_lazy('mask_list')
+
+
+class MaskUpdateView(UpdateView):
+    model = Mask
+    fields = '__all__'
+    template_name = 'mask/mask_form.html'
+    success_url = reverse_lazy('mask_list')
+
+
+class MaskDeleteView(DeleteView):
+    model = Mask
+    template_name = 'mask/mask_confirm_delete.html'
+    reverse_url = reverse_lazy('mask_list')
+
+
+class ADPMultiListView(ListView):
+    model = ADPMulti
+    template_name = 'adpmulti/adpmulti_list.html'
+    context_object_name = 'adp_multis'
+
+
+class ADPMultiCreateView(CreateView):
+    model = ADPMulti
+    fields = '__all__'
+    template_name = 'adpmulti_form.html'
+    success_url = reverse_lazy('adpmulti_list')
+
+
+class ADPMultiUpdateView(UpdateView):
+    model = ADPMulti
+    fields = '__all__'
+    template_name = 'adpmulti_form.html'
+    success_url = reverse_lazy('adpmulti_list')
+
+
+class ADPMultiDeleteView(DeleteView):
+    model = ADPMulti
+    template_name = 'adpmulti_confirm_delete.html'
+    reverse_url = reverse_lazy('adpmulti_list')
+
+
+class ADPSingleListView(ListView):
+    model = ADPSingle
+    template_name = 'adpsingle/adpsingle_list'
+    context_object_name = 'adp_singles'
+
+
+class ADPSingleCreateView(CreateView):
+    model = ADPSingle
+    fields = '__all__'
+    template_name = 'adpsingle/adpsingle_form.html'
+    success_url = reverse_lazy('adpsingle_list')
+
+
+class ADPSingleUpdateView(UpdateView):
+    model = ADPSingle
+    fields = '__all__'
+    template_name = 'adpsingle/adpsingle_form.html'
+    success_url = reverse_lazy('adpsingle_list')
+
+
+class ADPSingleDeleteView(DeleteView):
+    model = ADPSingle
+    template_name = 'adpsingle/adpsingle_confirm_delete.html'
+    success_url = reverse_lazy('adpsingle_list')
+
+
+class AirTankListView(ListView):
+    model = AirTank
+    template_name = 'airtank/airtank_list'
+    context_object_name = 'airtanks'
+
+
+class AirTankCreateView(CreateView):
+    model = AirTank
+    fields = '__all__'
+    template_name = 'airtank/airtank_form.html'
+    success_url = reverse_lazy('airtank_list')
+
+class AirTankUpdateView(UpdateView):
+    model = AirTank
+    fields = '__all__'
+    template_name = 'airtank/airtank_form.html'
+
+
+class AirTankDeleteView(DeleteView):
+    model = AirTank
+    template_name = 'airtank/airtank_confirm_delete.html'
+    success_url = reverse_lazy('airtank_list')
+
+
+class PCHOListView(ListView):
+    model = PCHO
+    template_name = 'pcho/pcho_list.html'
+    context_object_name = 'pchos'
+
+
+class PCHOCreateView(CreateView):
+    model = PCHO
+    fields = '__all__'
+    template_name = 'pcho/pcho_form.html'
+    success_url = reverse_lazy('pcho_list')
+
+
+class PCHOUpdateView(UpdateView):
+    model = PCHO
+    fields = '__all__'
+    template_name = 'pcho/pcho_form.html'
+    success_url = reverse_lazy('pcho_list')
+
+
+class PCHODeleteView(DeleteView):
+    model = PCHO
+    template_name = 'pcho/pcho_confirm_delete.html'
+    success_url = reverse_lazy('pcho_list')
+
+
+class PAListView(ListView):
+    model = PA
+    template_name = 'pa/pa_list.html'
+    context_object_name = 'pas'
+
+
+class PACreateView(CreateView):
+    model = PA
+    fields = '__all__'
+    template_name = 'pa/pa_form.html'
+    success_url = reverse_lazy('pa_list')
+
+
+class PAUpdateView(UpdateView):
+    model = PA
+    fields = '__all__'
+    template_name = 'pa/pa_form.html'
+    success_url = reverse_lazy('pa_list')
+
+
+class PADeleteView(DeleteView):
+    model = PA
+    template_name = 'pa/pa_confirm_delete.html'
+    success_url = reverse_lazy('pa_list')
+
+
+class CompleteListView(ListView):
+    model = Complete
+    template_name = 'complete/complete_list.html'
+    context_object_name = 'completes'
+
+
+class CompleteCreateView(CreateView):
+    model = Complete
+    fields = '__all__'
+    template_name = 'complete/complete_form.html'
+    success_url = reverse_lazy('complete_list')
+
+
+class CompleteUpdateView(UpdateView):
+    model = Complete
+    fields = '__all__'
+    template_name = 'complete/complete_form.html'
+    success_url = reverse_lazy('complete_list')
+
+
+class CompleteDeleteView(DeleteView):
+    model = Complete
+    template_name = 'complete/complete_confirm_delete.html'
+    success_url = reverse_lazy('complete_list')
+
+
+class StationEquipmentListView(DetailView):
+    model = Station
+    template_name = 'equipment/station_equipment.html'
+    context_object_name = 'station'
+
+    def get_context_data(self, **kwargs):
+        context = super(StationEquipmentListView, self).get_context_data(**kwargs)
+        station = self.get_object()
+
+        context['equipment_sections'] = [
+            ("Masky", Mask.objects.filter(located=station), 'mask_edit', 'mask_delete',
+             ['type', 'e_number', 'serial_number', 'status']),
+            ("ADP Multi", ADPMulti.objects.filter(located=station), 'adpmulti_edit', 'adpmulti_delete',
+             ['type', 'e_number', 'serial_number', 'status']),
+            ("ADP Single", ADPSingle.objects.filter(located=station), 'adpsingle_edit', 'adpsingle_delete',
+             ['type', 'e_number', 'serial_number', 'status']),
+            ("Tlakové lahve", AirTank.objects.filter(located=station), 'airtank_edit', 'airtank_delete',
+             ['serial_number', 'rev_date', 'status']),
+            ("Chemické obleky", PCHO.objects.filter(located=station), 'pcho_edit', 'pcho_delete',
+             ['type', 'serial_number', 'rev_date', 'status']),
+            ("PA", PA.objects.filter(located=station), 'pa_edit', 'pa_delete', ['type', 'serial_number', 'status']),
+        ]
+
+        return context
+
+
--- protiplyn/urls.py
+++ protiplyn/urls.py
@@ -35,4 +35,53 @@ urlpatterns = [
     path('cities/add/', CityCreateView.as_view(), name='city_add'),
 
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    # VehicleStorage
+    path('vehicles/', VehicleStorageListView.as_view(), name='vehicle_list'),
+    path('vehicles/add/', VehicleStorageCreateView.as_view(), name='vehicle_add'),
+    path('vehicles/<int:pk>/edit/', VehicleStorageUpdateView.as_view(), name='vehicle_edit'),
+    path('vehicles/<int:pk>/delete/', VehicleStorageDeleteView.as_view(), name='vehicle_delete'),
+
+    # Mask
+    path('masks/', MaskListView.as_view(), name='mask_list'),
+    path('masks/add/', MaskCreateView.as_view(), name='mask_add'),
+    path('masks/<int:pk>/edit/', MaskUpdateView.as_view(), name='mask_edit'),
+    path('masks/<int:pk>/delete/', MaskDeleteView.as_view(), name='mask_delete'),
+
+    # ADPMulti
+    path('adpmulti/', ADPMultiListView.as_view(), name='adpmulti_list'),
+    path('adpmulti/add/', ADPMultiCreateView.as_view(), name='adpmulti_add'),
+    path('adpmulti/<int:pk>/edit/', ADPMultiUpdateView.as_view(), name='adpmulti_edit'),
+    path('adpmulti/<int:pk>/delete/', ADPMultiDeleteView.as_view(), name='adpmulti_delete'),
+
+    # ADPSingle
+    path('adpsingle/', ADPSingleListView.as_view(), name='adpsingle_list'),
+    path('adpsingle/add/', ADPSingleCreateView.as_view(), name='adpsingle_add'),
+    path('adpsingle/<int:pk>/edit/', ADPSingleUpdateView.as_view(), name='adpsingle_edit'),
+    path('adpsingle/<int:pk>/delete/', ADPSingleDeleteView.as_view(), name='adpsingle_delete'),
+
+    # AirTank
+    path('airtanks/', AirTankListView.as_view(), name='airtank_list'),
+    path('airtanks/add/', AirTankCreateView.as_view(), name='airtank_add'),
+    path('airtanks/<int:pk>/edit/', AirTankUpdateView.as_view(), name='airtank_edit'),
+    path('airtanks/<int:pk>/delete/', AirTankDeleteView.as_view(), name='airtank_delete'),
+
+    # PCHO
+    path('pcho/', PCHOListView.as_view(), name='pcho_list'),
+    path('pcho/add/', PCHOCreateView.as_view(), name='pcho_add'),
+    path('pcho/<int:pk>/edit/', PCHOUpdateView.as_view(), name='pcho_edit'),
+    path('pcho/<int:pk>/delete/', PCHODeleteView.as_view(), name='pcho_delete'),
+
+    # PA
+    path('pa/', PAListView.as_view(), name='pa_list'),
+    path('pa/add/', PACreateView.as_view(), name='pa_add'),
+    path('pa/<int:pk>/edit/', PAUpdateView.as_view(), name='pa_edit'),
+    path('pa/<int:pk>/delete/', PADeleteView.as_view(), name='pa_delete'),
+
+    # Complete
+    path('completes/', CompleteListView.as_view(), name='complete_list'),
+    path('completes/add/', CompleteCreateView.as_view(), name='complete_add'),
+    path('completes/<int:pk>/edit/', CompleteUpdateView.as_view(), name='complete_edit'),
+    path('completes/<int:pk>/delete/', CompleteDeleteView.as_view(), name='complete_delete'),
+
+    path('stations/<int:pk>/equipment/', StationEquipmentListView.as_view(), name='station_equipment'),
 ]
