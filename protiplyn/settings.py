16511b0bd8fc62136f82610615933006fa4d35e0
TMAREK <tmarek@seznam.cz>
2025-06-22 19:40:54 +0200

    add logo.jpg for header.html

--- protiplyn/settings.py
+++ protiplyn/settings.py
@@ -121,6 +121,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # kde mam ulozene vlastni staticke soubory
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
